2df116b5f46f7984ab5b592289ce022e4b2b20bf
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-03-05 13:23:59 +0530

    Create 03-Coding Exercise.py

---
+++ 03-Coding Exercise.py
@@ -0,0 +1,9 @@
+#Convert bytes to a string.Save the result in the converted_str variable.
+
+#Expected Result:
+#'Python'
+
+bytes_to_convert ="Python"
+converted_str = str(bytes_to_convert)
+
+#Output: 'Python'
