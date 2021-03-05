d334e31cbb73e7f23875f8110b1d33b29cfe2f2d
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-03-05 16:29:54 +0530

    Create 12-Coding Exercise.py

---
+++ 12-Coding Exercise.py
@@ -0,0 +1,11 @@
+#Trim spaces from the variable 'word'. Save the result in the 'new_word' variable.
+
+#Expected result:
+#hello
+
+word = ' hello   '
+new_word = word.strip()
+print(new_word)
+
+
+#Output: 'hello'
