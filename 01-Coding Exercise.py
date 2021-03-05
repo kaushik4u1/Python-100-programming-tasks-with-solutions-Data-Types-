12f268ddeea7538121aacd8278326c7e6e9d6832
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-03-05 13:11:25 +0530

    Create 01-Coding Exercise.py

---
+++ 01-Coding Exercise.py
@@ -0,0 +1,11 @@
+#Split words from the sentence variable and put them in list arr.
+
+#Expected Result:
+#['I', 'am', 'learning', 'python']
+
+sentence = 'I am learning Python'
+
+arr = sentence.split()
+print(arr)
+
+#Output: ['I', 'am', 'learning', 'Python'] 
