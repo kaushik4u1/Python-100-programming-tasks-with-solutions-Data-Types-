e72f70fb6796a52b2bf341d2e235b473958e574a
Kaushik Mishra <mishrakaushik4@gmail.com>
2021-03-05 13:27:06 +0530

    Create 04-Coding Exercise.py

---
+++ 04-Coding Exercise.py
@@ -0,0 +1,10 @@
+#Get the word 'love' from the sentence variable. Save the result in the value variable.
+
+#Expected result:
+#'love'
+
+sentence = 'I love Python'
+er = sentence.split()
+value = er[1]
+
+#Output: 'love'
